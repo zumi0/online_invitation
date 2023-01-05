1f0584aecc35243c0ec30fcc8bd3afe10c3db36c
zumi0 <toshin.fish@gmail.com>
2023-01-05 23:58:44 +0900

    add files about firebase

---
+++ Firebase/client.ts
@@ -0,0 +1,31 @@
+import { initializeApp, getApps } from 'firebase/app';
+
+// 必要な機能をインポート
+import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { getAuth } from 'firebase/auth';
+import { getFunctions } from 'firebase/functions';
+
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSEGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+};
+
+if (!getApps()?.length) {
+  // Firebaseアプリの初期化
+  initializeApp(firebaseConfig);
+}
+
+// 他ファイルで使うために機能をエクスポート
+export const analytics = getAnalytics();
+export const db = getFirestore();
+export const storage = getStorage();
+export const auth = getAuth();
+export const funcions = getFunctions();
